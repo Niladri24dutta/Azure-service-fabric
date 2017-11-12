7e7b0b36d3f4fc0faf37256da9de14c1d341de4f
Niladri Dutta <nild2141@gmail.com>
2017-11-12 18:30:48 +0000

    add new .NET Core API

---
+++ Student/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Actors.Client;
+using Student.Interfaces;
+
+namespace Student
+{
+    
+    [StatePersistence(StatePersistence.Persisted)]
+    internal class Student : Actor, IStudent
+    {
+        /// <summary>
+        /// Initializes a new instance of Student
+        /// </summary>
+        /// <param name="actorService">The Microsoft.ServiceFabric.Actors.Runtime.ActorService that will host this actor instance.</param>
+        /// <param name="actorId">The Microsoft.ServiceFabric.Actors.ActorId for this actor instance.</param>
+        public Student(ActorService actorService, ActorId actorId)
+            : base(actorService, actorId)
+        {
+        }
+
+        /// <summary>
+        /// This method is called whenever an actor is activated.
+        /// An actor is activated the first time any of its methods are invoked.
+        /// </summary>
+        protected override Task OnActivateAsync()
+        {
+            ActorEventSource.Current.ActorMessage(this, "Actor activated.");
+            return this.StateManager.TryAddStateAsync("count", 0);
+        }
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <returns></returns>
+        Task<int> IStudent.GetCountAsync(CancellationToken cancellationToken)
+        {
+            return this.StateManager.GetStateAsync<int>("count", cancellationToken);
+        }
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        Task IStudent.SetCountAsync(int count, CancellationToken cancellationToken)
+        {
+            // Requests are not guaranteed to be processed in order nor at most once.
+            // The update function here verifies that the incoming count is greater than the current count to preserve order.
+            return this.StateManager.AddOrUpdateStateAsync("count", count, (key, value) => count > value ? count : value, cancellationToken);
+        }
+    }
+}
